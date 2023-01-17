b8000caeb5f9eee17b2151834116920f41b41da6
Joe Block <jpb@unixorn.net>
2023-01-16 19:56:08 -0700

    Add restic article

---
+++ resources/truenas-restic/example-settings.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Here are our backup settings
+#
+export B2_ACCOUNT_ID='your_b2_account_id'
+export B2_ACCOUNT_KEY='your_b2_key'
+
+# Use different directory prefixes for each backup repo
+# so they can share a bucket without interference
+export RESTIC_REPOSITORY='b2:your-restic-backups-bucket:dir-prefix'
+
+# This is used as the encryption key for your backups. If you lose it,
+# you won't be able to restore anything. I keep a copy of mine in my
+export RESTIC_PASSWORD='your-encryption-key'
+
+# Comment this out if you don't need an exclude file
+export EXCLUDE_FILE="--exclude-file=example-excludes"
+#export DRYRUN='--dry-run'
+
+# What paths do we want to back up? Remember to use the paths as seen inside
+# the jail, not the paths as seen in your TrueNAS environment
+export BACKUP_PATHS="/mnt/path-inside-jail/share /mnt/path-inside-jail/anothershare"
+
+
+# How many snapshots do we want to keep around?
+export MINIMUM_SNAPSHOTS_RETAINED=4
+
+export HOURS_RETAINED=48
+export DAYS_RETAINED=14
+export WEEKS_RETAINED=8
+export MONTHS_RETAINED=12
+export YEARS_RETAINED=5
